a0ab7bb0e5dc11efddab45b15f9acf5dec96c7b2
acmvnrvjiet <57917661+acmvnrvjiet@users.noreply.github.com>
2020-07-19 19:54:26 +0530

    Update jumping-numbers.cpp

--- jumping-numbers.cpp
+++ jumping-numbers.cpp
@@ -1,5 +1,7 @@
 /*https://practice.geeksforgeeks.org/problems/jumping-numbers/0
-Problem Statement : 
+Problem Statement : Given a positive number X. Find all Jumping Numbers smaller than or equal to X. 
+Jumping Number: A number is called Jumping Number if all adjacent digits in it differ by only 1. 
+                All single digit numbers are considered as Jumping Numbers. For example 7, 8987 and 4343456 are Jumping numbers but 796 and 89098 are not.
 
 Solution's Author : Madhurendra Tripathy
 
